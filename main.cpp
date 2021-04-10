4ed1fea628cf21aee4dee51ca6fd65e705a9117c
CLL612 <1813541@mail.nankai.edu.cn>
2021-04-10 17:16:31 +0800

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+﻿#include"common.h"
+#include"test.h"
+int main(int argc, char** argv)
+{
+#ifdef DEBUG
+    test_shudu();
+#endif
+    bool ret = ui_loop(argc, argv);
+    if (!ret) {
+        cout << "请输入正确的格式!!!" << endl;
+        return 0;
+    }
+    else {
+        return 0;
+    }
+}
